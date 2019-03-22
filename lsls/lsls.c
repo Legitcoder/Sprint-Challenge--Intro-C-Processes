c64d4e00b2ca05ccc060f7c2fc82703e8f325251
Moin Uddin <U_Moin75@yahoo.com>
2019-03-22 12:37:55 -0400

    Complete MVP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,50 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    DIR *dir;
+    struct dirent *ent;
+    size_t size = 10;
+    char *user_input = (char *)malloc(size);
+    
+    scanf("%[^\n]s", user_input);
 
-  // Open directory
+//    //Attempted to Reallocate char size dynamically based
+//    //on user input
+//    size_t length = strlen(user_input);
+//
+//    if(length > size) {
+//        printf("Input Exceeds 10 Characters, Reallocating Memory..\n");
+//        user_input = realloc(user_input, length);
+//    }
+//
+//    printf("%zu\n",strlen(user_input));
 
-  // Repeatly read and print entries
 
-  // Close directory
+    // Open directory
+    dir = opendir(user_input);
+    if(dir == NULL) {
+        printf("Can't open directory.\n");
+    }
 
-  return 0;
+    // Repeatly read and print entries
+    while((ent=readdir(dir)) != NULL) {
+        const char *entry = ent->d_name;
+        struct stat buf;
+        stat(entry, &buf);
+        printf("%10lld %s\n", buf.st_size, entry);
+    }
+
+    // Close directory
+    closedir(dir);
+
+    return 0;
 }
+
